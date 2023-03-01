e9afdbda49361fdc3bfc142c3380b5edc6c41ff6
Josue Fleitas <josue.fle.sanc@gmail.com>
2023-03-01 12:33:03 +0000

    Refresh

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "7caaef6c0d2968176f48460ce6fb394d",
-"/": "7caaef6c0d2968176f48460ce6fb394d",
+  "index.html": "01597aadf5d78bd9b467edf6e76d6ba8",
+"/": "01597aadf5d78bd9b467edf6e76d6ba8",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"main.dart.js": "f2192099f4868afa2448964a86f636bc",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"main.dart.js": "1ede6eb458a54fea50d258225c5eb70f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
